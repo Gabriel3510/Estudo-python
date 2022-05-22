4a2a19401bff99bca2c4dffbb8a7cce08716a0e0
Gabriel Lima <gabriel.lima@speedbird.aero>
2022-05-22 13:07:33 -0300

    Aulas sobre len e conveter numeros.

---
+++ aula27.py
@@ -0,0 +1,16 @@
+'''
+usuario = input('Digite seu usuário: ')
+qtd_caracteres= len(usuario)
+
+#print(usuario, qtd_caracteres, type(qtd_caracteres))
+if qtd_caracteres <6:
+    print('Voce precisa digitar pelo menos 6 caracteres')
+else:
+    print('Voce foi cadastrado no sistema.')
+'''
+
+string1 = input('Digite alguma coisa: ')
+string2 = input('Digite outra coisa: ')
+
+#print(f'A quantidade total de caracteres digitados foi {len(string1) + len(string2)}')
+print(string2.__len__())
